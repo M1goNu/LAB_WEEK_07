412cb9f640176e9d062416bea03c37c64155c136
M1goNu <rifqi.aldino71@gmail.com>
2025-10-08 11:02:38 +0700

    Commit No. 2 – Add Google Maps API Key and pinpoint current user location

--- app/src/main/java/com/example/lab_week_07/MapsActivity.kt
+++ app/src/main/java/com/example/lab_week_07/MapsActivity.kt
@@ -5,14 +5,19 @@ import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.location.Location
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.lab_week_07.databinding.ActivityMapsBinding
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,13 +26,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //This is the variable through which we will launch the permission request and track user responses
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
+    //A google play location service which helps us interact with Google's Fused Location Provider API
+    //The API intelligently provides us with the device location information
+    private val fusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Mendapatkan SupportMapFragment dan menyiapkan callback
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -76,9 +86,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         return ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED
     }
-
+    //Executed when the location permission has been granted by the user
     private fun getLastLocation() {
-        Log.d("MapsActivity", "getLastLocation() called.")
+        if (hasLocationPermission()) {
+            try {
+                fusedLocationProviderClient.lastLocation
+                    .addOnSuccessListener { location: Location? -> location?.let {
+                            val userLocation = LatLng(location.latitude,
+                                location.longitude)
+                            updateMapLocation(userLocation)
+                            addMarkerAtLocation(userLocation, "You")
+                        }
+                    }
+            } catch (e: SecurityException) {
+                Log.e("MapsActivity", "SecurityException: ${e.message}")
+            }
+        } else {
+            // If permission was rejected
+            requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+
+    private fun updateMapLocation(location: LatLng) {
+        mMap.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+            location, 7f))
+    }
+
+    private fun addMarkerAtLocation(location: LatLng, title: String) {
+        mMap.addMarker(
+            MarkerOptions().title(title)
+            .position(location))
     }
 
     //This is used to bring up a rationale dialog which will be used to ask the user for permission again
